fcbbc7dab837efb2afd0251854ec5747c6c23131
Lokendra <gclokendra10@gmail.com>
2025-06-23 21:02:39 +0545

    wip:posts fetched based on tag

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -22,41 +22,25 @@ export default function Home() {
       email: string;
       avatar?: string | null | undefined;
       username: string;
-      created_at: string;
+      created_at: string | undefined;
 
     }
   }
 
-  interface TagData {
+  interface UserData {
     id: number;
-    tag_name: string;
-    image?: string | null | undefined;
-    short_description?: string;
-    created_at: string | Date;
-    user: {
-      id: number;
-      name: string;
-      email: string;
-      avatar?: string | null | undefined;
-      username: string;
-      created_at: string;
+    name: string;
+    email: string;
+    avatar?: string | null | undefined;
+    username: string;
+    created_at: string;
 
-    },
-    posts: {
-      id: number;
-      title: string;
-      content: string;
-      feature_image?: string | null | undefined;
-      short_description?: string;
-      created_at: string | Date;
-    },
   }
-
 
   const APP_URL = `${process.env.NEXT_PUBLIC_API_URL}`;
   const { authToken, isLoading } = myAppHook();
   const [posts, setPosts] = useState<PostData[]>([]);
-  const [user, setUser] = useState<TagData['user'] | null>(null);
+  const [user, setUser] = useState<UserData | null>(null);
   const [selectedTagId, setSelectedTagId] = useState<number | null>(null);
   const [tagPosts, setTagPosts] = useState<PostData[]>([]);
 
@@ -75,6 +59,7 @@ export default function Home() {
       // console.log(response.data.data.data.user);
       setPosts(response.data.data.data);
       setUser(response.data.data.data.user);
+      console.log(user);
     } catch (error) {
       console.log(error);
     }
@@ -131,7 +116,7 @@ export default function Home() {
   const IMAGE_URL = `${process.env.NEXT_PUBLIC_POST_IMAGE_BASE_URL}`;
   const AVATAR_URL = `${process.env.NEXT_PUBLIC_IMAGE_BASE_URL}/`;
 
-  // console.log(posts);
+  // console.log(user?.created_at);
 
 
   return (
@@ -169,9 +154,9 @@ export default function Home() {
                 <button
                   key={tag.id}
                   onClick={() => fetchPostByTag(tag.id)}
-                  className={`
+                  className={`cursor-pointer
       ${selectedTagId === tag.id ?
-                      'border-black dark:border-white' :
+                      'border-b-2 border-black dark:border-white' :
                       'border-transparent'
                     }
     `}
@@ -181,19 +166,13 @@ export default function Home() {
               ))}
 
 
-
-
-              {/* <span className="bg-green-100 text-green-800 dark:bg-green-900 dark:text-green-300 text-xs px-2 rounded">
-                New
-              </span> */}
-
             </nav>
           </div>
 
           {/* Right Arrow */}
           <button
             onClick={() => scrollNav('right')}
-            className="absolute py-4 right-0 top-1/2 -translate-y-1/2 z-20 px-3 
+            className="cursor-pointer absolute py-4 right-0 top-1/2 -translate-y-1/2 z-20 px-3 
              bg-white dark:bg-gray-900 
              shadow-none dark:shadow 
              hover:bg-white dark:hover:bg-gray-800 
@@ -208,25 +187,28 @@ export default function Home() {
 
         {/* Blog Post Card */}
         {
-          (selectedTagId ? tagPosts : posts).map((post) => (
+
+          selectedTagId ? tagPosts.map((post) => (
+
             <div key={post.id} className="flex justify-between items-start py-6 border-b dark:border-gray-700">
               {/* Content Section */}
               <div className="flex-1 pr-4">
                 {/* Author */}
                 <div className="flex gap-3 items-center text-sm text-gray-500 dark:text-gray-400">
-                
+
                   <Image
                     src={
-                      post.user.avatar && typeof post.user.avatar === 'string' && post.user.avatar.length > 0
-                        ? AVATAR_URL + post.user.avatar
-                        : `https://ui-avatars.com/api/?name=${encodeURIComponent(post.user.name)}&background=random`
+                      user?.avatar && typeof user.avatar === 'string' && user.avatar.length > 0
+                        ? AVATAR_URL + user.avatar
+                        : `https://ui-avatars.com/api/?name=${encodeURIComponent(user?.name || 'U')}&background=random`
                     }
                     width={25}
                     height={25}
                     className="rounded-full"
-                    alt={post.user?.name}
+                    alt={user?.name || 'User'}
                   />
-                  {post.user.name}</div>
+                  {user?.name}
+                </div>
 
                 {/* Title */}
                 <h2 className="cursor-pointer text-xl font-bold text-gray-900 dark:text-white mt-1 break-words leading-snug">
@@ -242,7 +224,7 @@ export default function Home() {
                 <div className="flex items-center justify-between mt-3 text-sm text-gray-500 dark:text-gray-400">
                   {/* Left Side: Meta Info */}
                   <div className="flex space-x-4">
-                    <span>{formatToMonthDay(post.user?.created_at)}</span>
+                    <span>{formatToMonthDay(user?.created_at || '')}</span>
                     <span className="flex items-center gap-1">
                       <svg
                         xmlns="http://www.w3.org/2000/svg"
@@ -297,8 +279,110 @@ export default function Home() {
                 />
               </div>
             </div>
-          ))
+
+          )) :
+
+            posts.map((post) => (
+
+              <div key={post.id} className="flex justify-between items-start py-6 border-b dark:border-gray-700">
+                {/* Content Section */}
+                <div className="flex-1 pr-4">
+                  {/* Author */}
+                  <div className="flex gap-3 items-center text-sm text-gray-500 dark:text-gray-400">
+
+                    <Image
+                      src={
+                        post.user?.avatar && typeof post.user.avatar === 'string' && post.user.avatar.length > 0
+                          ? AVATAR_URL + post.user.avatar
+                          : `https://ui-avatars.com/api/?name=${encodeURIComponent(post.user?.name || 'U')}&background=random`
+                      }
+                      width={25}
+                      height={25}
+                      className="rounded-full"
+                      alt={post.user?.name || 'User'}
+                    />
+                    {post.user.name}
+                  </div>
+
+                  {/* Title */}
+                  <h2 className="cursor-pointer text-xl font-bold text-gray-900 dark:text-white mt-1 break-words leading-snug">
+                    {post.title}
+                  </h2>
+
+                  {/* Description */}
+                  <p className="text-sm text-gray-600 dark:text-gray-300 mt-2 line-clamp-2">
+                    {parse(trimWords(post.content, 5))}
+                  </p>
+
+                  {/* Meta Info */}
+                  <div className="flex items-center justify-between mt-3 text-sm text-gray-500 dark:text-gray-400">
+                    {/* Left Side: Meta Info */}
+                    <div className="flex space-x-4">
+                      <span>{formatToMonthDay(post.user?.created_at || '')}</span>
+                      <span className="flex items-center gap-1">
+                        <svg
+                          xmlns="http://www.w3.org/2000/svg"
+                          className="cursor-pointer h-4 w-4 text-red-500"
+                          fill="currentColor"
+                          viewBox="0 0 24 24"
+                        >
+                          <path d="M12 21.35l-1.45-1.32C5.4 15.36 2 12.28 2 8.5 2 5.42 
+             4.42 3 7.5 3c1.74 0 3.41 0.81 
+             4.5 2.09C13.09 3.81 14.76 3 
+             16.5 3 19.58 3 22 5.42 22 
+             8.5c0 3.78-3.4 6.86-8.55 
+             11.54L12 21.35z" />
+                        </svg>
+                        2.8K
+                      </span>
+
+                      <span className="cursor-pointer">💬 72</span>
+                    </div>
+
+
+
+                    {/* Right Side: Save Icon */}
+                    <button className="text-gray-500 cursor-pointer mr-[80px] hover:text-black dark:text-gray-400 dark:hover:text-white">
+                      <svg
+                        xmlns="http://www.w3.org/2000/svg"
+                        className="h-5 w-5"
+                        fill="none"
+                        viewBox="0 0 24 24"
+                        stroke="currentColor"
+                        strokeWidth={2}
+                      >
+                        <path
+                          strokeLinecap="round"
+                          strokeLinejoin="round"
+                          d="M5 5v14l7-5 7 5V5a2 2 0 00-2-2H7a2 2 0 00-2 2z"
+                        />
+                      </svg>
+                    </button>
+                  </div>
+
+                </div>
+
+                {/* Feature Image */}
+                <div className="min-w-[128px] h-full">
+                  <Image
+                    src={`${post.feature_image ? IMAGE_URL + post.feature_image : 'noimage.svg'}`}
+                    alt="Feature"
+                    width={150}
+                    height={150}
+                    className="rounded-md"
+                  />
+                </div>
+              </div>
+
+            ))
+
         }
+        
+        {selectedTagId && tagPosts && tagPosts.length === 0 && (
+          <div className="flex justify-between items-start py-6 border-b dark:border-gray-700">
+            <h3>No posts available</h3>
+          </div>
+        )}
 
 
 
